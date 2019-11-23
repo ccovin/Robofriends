9074a8829aa85a329c9b0fac5ea881bc60a6a09a
Chip Covin <chipcovin1@gmail.com>
2019-11-23 14:55:06 -0500

    Updates

---
+++ precache-manifest.b782f631fae4f43da26108293c02f8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d471a463385ab65e04f8c1d0dcfdd3",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b0c34d8d30c38fce9870",
+    "url": "/Robofriends/static/css/main.5e7c9cc1.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/Robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "b0c34d8d30c38fce9870",
+    "url": "/Robofriends/static/js/main.d856234d.chunk.js"
+  },
+  {
+    "revision": "a3eff8e1727534c00f37",
+    "url": "/Robofriends/static/js/runtime-main.11a3c7d6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
